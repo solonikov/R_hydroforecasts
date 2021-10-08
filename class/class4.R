3195f3f8eb99f2d3034d11209e7f4051dc7ae7ce
esmoreido <moreido@mail.ru>
2021-10-08 12:02:50 +0300

    Class 4 updated

--- class/class4.R
+++ class/class4.R
@@ -1,18 +1,21 @@
 Sys.setlocale("LC_ALL","Russian")
 library(ggplot2)
+library(readxl)
 
 kama <- read_xlsx('data/kama/kama_q.xlsx')
 
-ggplot(kama, aes(date, value, col=index)) + geom_line()
+ggplot(kama, aes(date, value, col=index)) + geom_line() +
+  facet_wrap(index~., scales = 'free_y', ncol = 1)
 
-
-setwd('d:/YandexDisk/ИВПРАН/R forecasts/байкал')
+getwd()
+setwd('data/baikal')
 # список файлов из рабочей директории определенного расширения
+list.files()
 xls_files <- list.files(pattern = '*.xls')
 xls_files
-library(readxl)
+
 # считываем один из файлов с учетом его структуры и требуемых столбцов
-prog_apr <- read_xls(xls_files[7], skip = 10, 
+prog_apr <- read_xls(xls_files[7], skip = 10,  
                      col_names = c('year', 'pred', 'pred1', 'obs'), 
                      col_types = c('numeric', 'numeric', 'skip', 'numeric'))
 
@@ -26,6 +29,11 @@ for (x in xls_files){
   print(dim(df))
   prog_df <- rbind(prog_df, df)
 }
+# добавляем столбец с названиями месяцев
+prog_df$month <- rep(month.abb[c(1, 10:12, 2:9)], each = 54)
+prog_df$month <- factor(prog_df$month, levels = month.abb, ordered = T)
+str(prog_df)
+
 
 # вариант 2: создаем функцию для считывания...
 read_prog <- function(x){
@@ -35,16 +43,20 @@ read_prog <- function(x){
 }
 # ...и применяем ее ко всем файлам с помощью lapply
 prog_list <- lapply(xls_files, read_prog)
+names(prog_list) <- xls_files
 # после чего превращаем из списка в датафрейм
 prog_list_df <- do.call(what = rbind, args = prog_list)
-
-# добавляем столбец с названиями месяцев
-prog_df$month <- rep(month.abb[c(1, 10:12, 2:9)], each = 54)
-prog_df$month <- factor(prog_df$month, levels = month.abb, ordered = T)
-str(prog_df)
-
+prog_list_df$month <- rownames(prog_list_df)
+rownames(prog_list_df) <- NULL
+prog_list_df$month <- substr(x = prog_list_df$month,
+       start = regexpr("[0-9]",prog_list_df$month), 
+      stop = regexpr(".xls",prog_list_df$month)-1)
 summary(prog_df)
 save(prog_df, file = 'prog_df.RData')
+
+ggplot(prog_list_df, aes(obs, pred, col=month)) + 
+  geom_point() + facet_wrap(month~., scales = 'free')
+
 # визуальная оценка 
 library(ggplot2)
 # линии

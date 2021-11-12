8be0f3d4ddd7d0a5a6438cfeeb59fc8facc7e138
moonmodule <kusok.stanley@yandex.ru>
2021-11-12 09:40:44 +0300

    ...

--- class/select_experiment.R
+++ class/select_experiment.R
@@ -2,48 +2,72 @@ Sys.setlocale("LC_ALL","Russian")
 library(sp)
 library(sf)
 library(leaflet)
-setwd('c:/Users/morey/Documents/R/rforecast/data')
+setwd('C:/R_pr/hydr_forecasts/rforecast/data')
 gmvo <- read.csv('year_count_gmvo.csv')
 hydropost <- read.table(file = 'np_hydropost.csv', sep = ";", header = T, check.names = F, stringsAsFactors = F)
 df <- merge(hydropost, gmvo, by = 'index')
 
-leaflet(hydropost) %>%
+df$size = cut(df$f, labels = c('Очень малая', 'Малая', 'Средняя', 'Большая'),
+              breaks = c(-Inf, 2000, 20000, 50000, Inf), ordered_result = T)
+
+pal = colorFactor(palette = 'Spectral', domain = df$size)
+
+leaflet(df) %>%
   addTiles() %>%
-  addMarkers(~lon, ~lat, popup = ~name)
+  addCircleMarkers(~lon, ~lat,
+                   popup = ~paste(name, f, sep = ', '),
+                   radius = 2,
+                   color = ~pal(size),
+                   opacity = 0.5)
 
-bins <-c(0, 1000, 2000, 5000, 10000, 50000, 100000, 500000, 3000000) 
-pal <- colorBin("YlGnBu", bins=bins, na.color = "#aaff56", reverse = T)
+small_r = df[df$size == 'Очень малая',]
+sp_posts = st_as_sf(small_r, coords = c('lon', 'lat'), crs = 4326)
+st_crs(sp_posts)
+sp_posts <- st_transform(sp_posts, crs = 3857)
+buf = st_buffer(sp_posts, 50000)
 
-leaflet(hydropost) %>%
-  addTiles() %>%
-  addCircleMarkers(~lon, ~lat, popup = ~paste(name, f, sep = ', '), radius = 1,  
-                   color = ~pal(f), opacity = 1, fillColor =~pal(f))
-
-leaflet(hydropost) %>%
-  addTiles() %>%
-  addCircleMarkers(~lon, ~lat, popup = ~paste(name, f, sep = ', '), radius = 1, color = ~pal(f),  opacity = 1, fillColor =~pal(f)) %>%
-  addLegend(pal = pal, values = ~f,
-            title = "Площадь водосбора",
-            opacity = 1)
-
-leaflet(hydropost[hydropost$f <= 2000 & hydropost$lon <= 60,]) %>%
-  addTiles() %>%
-  addCircleMarkers(~lon, ~lat, popup = ~paste(name, f, sep = ', '), radius = 1, color = ~pal(f),  opacity = 1, fillColor =~pal(f)) %>%
-  addLegend(pal = pal, values = ~f,
-            title = "Площадь водосбора",
-            opacity = 1)
-
-library(sf)
-sp_hydr <- st_as_sf(hydropost, coords = c('lon', 'lat'), crs = 4326)
-st_crs(sp_hydr)
-sp_hydr <- st_transform(sp_hydr, crs = 3857)
-st_crs(sp_hydr)
-buf <- st_buffer(sp_hydr, 10000)
+pal_buf = colorFactor(palette = 'Spectral', domain = buf$property)
 
 leaflet(st_transform(buf, crs = 4326)) %>%
   addTiles() %>%
   addPolygons() %>%
-  addMarkers(data = hydropost)
+  addMarkers(data = st_transform(sp_posts, crs = 4326))
+
+leaflet(st_transform(buf, crs = 4326)) %>%
+  addTiles() %>%
+  addPolygons(color = ~pal_buf(property),
+              opacity = 1) %>%
+  addLegend(pal = pal_buf, values = ~property,
+            title = "Принадлежность",
+            opacity = 1)
+
+syn_meta <- read.csv('np_meteost.csv')
+
+leaflet(st_transform(buf, crs = 4326)) %>%
+  addTiles() %>%
+  addPolygons(color = ~pal_buf(property),
+              opacity = 1) %>%
+  addLegend(pal = pal_buf, values = ~property,
+            title = "Принадлежность",
+            opacity = 1) %>%
+  addMarkers(data = syn_meta, lng = ~lon, lat = ~lat)
+
+intersect = st_intersection(buf, 
+                            st_transform(st_as_sf(syn_meta, 
+                                                       coords = c('lon', 'lat'), crs = 4326), crs = 3857))
+
+intersect_lonlat = st_transform(intersect, crs = 4326)
+
+
+leaflet(intersect_lonlat) %>%
+  addTiles() %>%
+  addCircleMarkers(label = ~paste0(index.1, ', ', name.1),
+                   fillColor = ~pal_buf(property),
+                   fillOpacity = 1,
+                   stroke = F,
+                   popup = ~paste(index, name, index.1, name.1, f, sep = ', '),
+                   radius = ~f*0.005)
+#-------------------------------------------------------------------------------
 
 syn_meta <- read.csv('np_meteost.csv')
 leaflet(hydropost[hydropost$f <= 2000 & hydropost$lon <= 60,]) %>%
@@ -61,4 +85,5 @@ inters_lonlat <- st_transform(intersc, crs = 4326)
 leaflet(inters_lonlat) %>%
   addTiles() %>%
   addCircleMarkers(color = 'Yellow') %>%
-  addMarkers(hydropost, lng = hydropost$lon, lat = hydropost$lat)
+  addMarkers(data = st_transform(sp_posts, 4326))
+  

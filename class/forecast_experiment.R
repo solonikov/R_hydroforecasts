2c1aa449a1c6e8406d647802707150242b742bb5
esmoreido <moreido@mail.ru>
2021-11-19 10:43:17 +0300

    fix tree

--- class/forecast_experiment.R
+++ class/forecast_experiment.R
@@ -2,31 +2,25 @@ Sys.setlocale("LC_ALL","Russian")
 library(dplyr)
 library(RPostgreSQL)
 library(readxl)
-library(reshape2)
 library(ggplot2)
-library(reshape2)
-
-rivers_list <- read_xlsx('data/rivers/rivers_list.xlsx')
 
 # гидрология
-load('data/rivers/river_data.RData')
+load('data/rivers/gmvo_data.RData')
 # метеорология
 load('data/rivers/weather_data.RData')
 
-unique(river_data$index)
-unique(weather_data$index)
 # выбираем
 my_q <- river_data %>%
-  filter(index == 82060)
+  filter(index == 78519)
 my_w <- weather_data %>%
-  filter(index == 37099)
+  filter(index == 34635)
 
 # соединяем
 df <- merge(my_q, my_w, by = 'date', suffixes = c('_hydro', '_meteo'))
 ggplot(df, aes(x=date)) + geom_line(aes(y=q, col='Q')) + geom_bar(aes(y=prec, fill='P'), fill = 'Blue', stat = 'identity')
 
 # автокорреляция
-acf(df$q, na.action = na.pass, plot = F, lag.max = 100)
+acf(df$q, na.action = na.pass)
 acf(df$prec, na.action = na.pass)
 
 # перемотка
@@ -38,13 +32,11 @@ lag_df <- df %>% mutate_at(vars(q), funs_(lag_set))
 q_cor <- cor(select(lag_df, contains('q')), 
              use = 'complete.obs')
 
-ggplot(melt(q_cor), aes(Var1, Var2, fill=value)) + 
-  geom_tile() + 
-  geom_text(aes(label=round(value, 2)), col='White')
+ggplot(melt(q_cor), aes(X1, X2, fill=value)) + geom_tile() + geom_text(aes(label=round(value, 2)), col='White')
 
 prec_lag_names <- paste('prec', sprintf(fmt = "%02d", lags), sep = "_")
 prec_lag_set <- setNames(paste("dplyr::lag(., ", lags, ")"), prec_lag_names)
 lag_df <- lag_df %>% mutate_at(vars(prec), funs_(prec_lag_set))
 
 q_cor <- cor(select(lag_df, contains(c('q', 'p'))), use = 'complete.obs')
-ggplot(melt(q_cor), aes(Var1, Var2, fill=value)) + geom_tile() + geom_text(aes(label=round(value, 2)), col='White')
+ggplot(melt(q_cor), aes(X1, X2, fill=value)) + geom_tile() + geom_text(aes(label=round(value, 2)), col='White')
--- class/select_experiment.R
+++ class/select_experiment.R
@@ -1,109 +1,89 @@
 Sys.setlocale("LC_ALL","Russian")
-# install.packages("sp", "sf", "leaflet")
 library(sp)
 library(sf)
 library(leaflet)
-getwd()
-setwd('c:/Users/morey/Documents/R_Projects/rforecast/data')
+setwd('C:/R_pr/hydr_forecasts/rforecast/data')
 gmvo <- read.csv('year_count_gmvo.csv')
-hydropost <- read.csv(file = 'np_hydropost.csv',encoding = "UTF-8", sep = ";", header = T, check.names = F, stringsAsFactors = F)
+hydropost <- read.table(file = 'np_hydropost.csv', sep = ";", header = T, check.names = F, stringsAsFactors = F)
 df <- merge(hydropost, gmvo, by = 'index')
-df$cat <- cut(x = df$f, breaks = c(-Inf, 2000, 20000, 50000, Inf), 
-    labels = c('очень малая', 'малая', 'средняя', 'большая'), 
-    ordered_result = T)
-summary(df)
-str(df)
+
+df$size = cut(df$f, labels = c('Очень малая', 'Малая', 'Средняя', 'Большая'),
+              breaks = c(-Inf, 2000, 20000, 50000, Inf), ordered_result = T)
+
+pal = colorFactor(palette = 'Spectral', domain = df$size)
 
 leaflet(df) %>%
   addTiles() %>%
-  addMarkers(~lon, ~lat, popup = ~name)
+  addCircleMarkers(~lon, ~lat,
+                   popup = ~paste(name, f, sep = ', '),
+                   radius = 2,
+                   color = ~pal(size),
+                   opacity = 0.5)
 
-# bins <-c(0, 1000, 2000, 5000, 10000, 50000, 100000, 500000, 3000000) 
-# pal <- colorBin("YlGnBu", bins=bins, na.color = "#aaff56", reverse = T)
+small_r = df[df$size == 'Очень малая',]
+sp_posts = st_as_sf(small_r, coords = c('lon', 'lat'), crs = 4326)
+st_crs(sp_posts)
+sp_posts <- st_transform(sp_posts, crs = 3857)
+buf = st_buffer(sp_posts, 50000)
 
-pal <- colorFactor(palette = 'Spectral', domain = df$cat)
-
-leaflet(df) %>%
-  addTiles() %>%
-  addCircleMarkers(~lon, ~lat, 
-                   popup = ~paste(name, f, sep = ', '), 
-                   radius = ~q,  
-                   fill = T, 
-                   fillColor = ~pal(cat), 
-                   fillOpacity = 1, stroke = T, 
-                   color = 'Black', weight = 0.5, 
-                   clusterOptions = markerClusterOptions())
-
-leaflet(df) %>%
-  addTiles() %>%
-  addCircleMarkers(~lon, ~lat, 
-                   popup = ~paste(name, f, sep = ', '), 
-                   radius = ~q*0.5,  
-                   fill = T, 
-                   fillColor = ~pal(cat), 
-                   fillOpacity = 1, stroke = T, 
-                   color = 'Black', weight = 0.5) %>%
-addLegend(pal = pal, values = ~cat, 
-            title = "Размер реки",
-            opacity = 1)
-
-small_r <- df[df$cat == 'очень малая',]
-
-sp_hydr <- st_as_sf(small_r, coords = c('lon', 'lat'), crs = 4326)
-st_crs(sp_hydr)
-sp_hydr <- st_transform(sp_hydr, crs = 3857)
-st_crs(sp_hydr)
-st_write(sp_hydr, 'sp_hydr.shp')
-buf <- st_buffer(sp_hydr, 50000)
+pal_buf = colorFactor(palette = 'Spectral', domain = buf$property)
 
 leaflet(st_transform(buf, crs = 4326)) %>%
   addTiles() %>%
   addPolygons() %>%
-  addMarkers(data = st_transform(sp_hydr, crs = 4326))
-
-pal1 <- colorFactor(palette = 'viridis', 
-                    domain = buf$property, 
-                    na.color = 'Red')
+  addMarkers(data = st_transform(sp_posts, crs = 4326))
 
 leaflet(st_transform(buf, crs = 4326)) %>%
   addTiles() %>%
-  addPolygons(color = ~pal1(property),
-              fill = T, 
-              fillColor = ~pal1(property), opacity = 1,
-              fillOpacity = 1, stroke = T) %>%
-  addLegend(pal = pal1, values = ~property, 
+  addPolygons(color = ~pal_buf(property),
+              opacity = 1) %>%
+  addLegend(pal = pal_buf, values = ~property,
             title = "Принадлежность",
             opacity = 1)
 
-syn_meta <- read.csv('np_meteost.csv', encoding = "UTF-8")
-summary(syn_meta)
+syn_meta <- read.csv('np_meteost.csv')
+
 leaflet(st_transform(buf, crs = 4326)) %>%
   addTiles() %>%
-  addPolygons(color = ~pal1(property),
-              fill = T, 
-              fillColor = ~pal1(property), opacity = 0.5,
-              fillOpacity = 0.5, stroke = T) %>%
-  addLegend(pal = pal1, values = ~property, 
+  addPolygons(color = ~pal_buf(property),
+              opacity = 1) %>%
+  addLegend(pal = pal_buf, values = ~property,
             title = "Принадлежность",
             opacity = 1) %>%
   addMarkers(data = syn_meta, lng = ~lon, lat = ~lat)
 
+intersect = st_intersection(buf, 
+                            st_transform(st_as_sf(syn_meta, 
+                                                       coords = c('lon', 'lat'), crs = 4326), crs = 3857))
 
-intersc <- st_intersection(buf, 
-                           st_transform(
-                             st_as_sf(syn_meta, 
-                                      coords = c('lon', 'lat'), 
-                                      crs = 4326), 
-                             crs = 3857))
+intersect_lonlat = st_transform(intersect, crs = 4326)
 
+
+leaflet(intersect_lonlat) %>%
+  addTiles() %>%
+  addCircleMarkers(label = ~paste0(index.1, ', ', name.1),
+                   fillColor = ~pal_buf(property),
+                   fillOpacity = 1,
+                   stroke = F,
+                   popup = ~paste(index, name, index.1, name.1, f, sep = ', '),
+                   radius = ~f*0.005)
+#-------------------------------------------------------------------------------
+
+syn_meta <- read.csv('np_meteost.csv')
+leaflet(hydropost[hydropost$f <= 2000 & hydropost$lon <= 60,]) %>%
+  addTiles() %>%
+  addCircleMarkers(~lon, ~lat, popup = ~paste(name, f, sep = ', '), color = ~pal(f)) %>%
+  addLegend(pal = pal, values = ~f,
+            title = "Площадь водосбора",
+            opacity = 1) %>%
+  addCircleMarkers(data = syn_meta[syn_meta$lon <= 60, ], lng = syn_meta$lon, lat = syn_meta$lat, 
+                   popup = syn_meta$name, color = 'Red', radius = 2) %>%
+  addPolygons(data = st_transform(buf, crs = 4326), color = 'Yellow')
+
+intersc <- st_intersection(buf, st_transform(st_as_sf(syn_meta, coords = c('lon', 'lat'), crs = 4326), crs = 3857))
 inters_lonlat <- st_transform(intersc, crs = 4326)
 leaflet(inters_lonlat) %>%
   addTiles() %>%
-  addCircleMarkers(radius = ~f * 0.01, fillColor = ~pal1(property), 
-                   fillOpacity = 1, 
-                   stroke = T, color = 'Black',
-                   opacity = 1, weight = 0.3,
-                   popup = ~paste(index, name, f, index.1, name.1, 
-                                  sep = ', '))
-
-
+  addCircleMarkers(color = 'Yellow') %>%
+  addMarkers(data = st_transform(sp_posts, 4326))
+  
